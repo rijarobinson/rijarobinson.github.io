46cc9d54e133c404c4d7e0b59b7266a124b355b7
Marija Robinson <marija@springmail.com>
2021-05-13 17:47:14 -0500

    Change company placeholders test

--- js/m-js.js
+++ js/m-js.js
@@ -1,1 +1,7 @@
-alert('hi');
+FC.client.on('ready.done', changeCompanyField);
+FC.client.on('render.done', changeCompanyField);
+ 
+function changeCompanyField () {
+  $('#fc #shipping_company').attr('placeholder','Company');
+  $('#fc #billing_company').attr('placeholder','Company');
+}
